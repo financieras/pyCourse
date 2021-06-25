95200f5e8c015b1dac900632ed91a599c8c9ee59
addolfo <financieras@gmail.com>
2021-06-25 09:53:55 +0200

    Primer ejemplo y propuesto

---
+++ Ejemplos/ejemplo1_10.py
@@ -0,0 +1,11 @@
+# Esto es un comentario del programador
+print(4+2*3)
+print(4+(2*3)) # la solución aquí también es 10
+print((4+2)*3) # la solución ahora es 18
+print("La raiz cuadrada de 16 es", 16**.5)
+
+'''Esto es un comentario
+que se escribe en
+varias líneas'''
+
+print("La raiz cuadrada de 16 es", 16**.5)
---
+++ Propuestos/propuesto1_10.py
@@ -0,0 +1,4 @@
+# Crear un archivo .py con unas cuantas operaciones aritméticas
+# algunas con uso de paréntesis
+# Ejecutar el archivo .py desde terminal
+# Ejecutar el archivo .py desde la terminal integrada en VSCode
