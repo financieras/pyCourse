1c6d4138810ef61cd28509bc877d810eaa37004f
addolfo <financieras@gmail.com>
2021-08-05 10:54:54 +0200

    encapsulamiento

--- jupyter/mypy/funciones_matematicas.py
+++ jupyter/mypy/funciones_matematicas.py
@@ -1,6 +1,11 @@
 def sumar(n1,n2):
     print(f"El resultado de la suma es {n1+n2}")
+    
 def restar(n1,n2):
     print(f"El resultado de la resta es {n1-n2}")
+
 def multiplicar(n1,n2):
     print(f"El resultado del producto es {n1*n2}")
+
+def _dividir(n1,n2):
+    print(f"El resultado de la división es {n1/n2}")
