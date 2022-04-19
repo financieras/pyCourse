125e1590b4980ca97595e7905448728626c8a5a0
financieras <financieras@gmail.com>
2022-04-19 20:17:02 +0200

    tilde

--- ejemplos/ejemplo1_0010.py
+++ ejemplos/ejemplo1_0010.py
@@ -2,10 +2,10 @@
 print(4+2*3)
 print(4+(2*3)) # la solución aquí también es 10
 print((4+2)*3) # la solución ahora es 18
-print("La raiz cuadrada de 16 es", 16**.5)
+print("La raíz cuadrada de 16 es", 16**.5)
 
 '''Esto es un comentario
 que se escribe en
 varias líneas'''
 
-print("La raiz cuadrada de 16 es", 16**.5)
+print("La raíz cuadrada de 16 es", 16**.5)
